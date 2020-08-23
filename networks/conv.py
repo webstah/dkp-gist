dc0c1bd030f653e247bb03ecf096a3341ed2c404
Matthew B. Webster <50574746+webstah@users.noreply.github.com>
2020-08-24 08:24:13 +0900

    Update conv.py

--- networks/conv.py
+++ networks/conv.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR
 
 from utils import DFATrainingHook, OutputTrainingHook, TestTrainingHook
 
