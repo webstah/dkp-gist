dc7aabba39df34284d0dc066c0949b493a62100e
Matthew B. Webster <50574746+webstah@users.noreply.github.com>
2020-08-25 02:57:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,6 @@ def test(model, device, test_loader, train_loader):
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
-            # data = data.view(data.shape[0], -1) #flatten the data
 
             output = model(data, None)
             one_hot = torch.nn.functional.one_hot(target, 10).float()
@@ -73,16 +72,18 @@ def test(model, device, test_loader, train_loader):
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='Pytorch Implementation of DFA and Learned Direct Connections')
-    parser.add_argument('--batch-size', type=int, default=50, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=100, metavar='N',
                         help='number of epochs to train (default: 14)')
-    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
-                        help='learning rate (default: 1.0)')
-    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
-                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--lr', type=float, default=5e-4, metavar='LR',
+                        help='learning rate (default: 5e-4)')
+    parser.add_argument('--b-lr', type=float, default=1e-4, metavar='BLR',
+                        help='learning rate for backward parameters (default: 1e-4)')
+    parser.add_argument('--gamma', type=float, default=0.8, metavar='M',
+                        help='Learning rate step gamma (default: 0.8)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
     parser.add_argument('--dry-run', action='store_true', default=False,
@@ -93,12 +94,12 @@ def main():
                         help='how many batches to wait before logging training status')
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
-    parser.add_argument('--train-mode', choices = ['BP', 'DKP', 'DFA'], default='BP',
+    parser.add_argument('--train-mode', choices = ['BP', 'DKP', 'DFA'], default='DKP',
                         help='Choose between backpropagation (BP), Direct Kolen Pollack (DKP), or Direct Feedback Alignment (DFA).')
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
-    # torch.manual_seed(args.seed)
+    torch.manual_seed(args.seed)
 
     device = torch.device("cuda" if use_cuda else "cpu")
 
@@ -111,14 +112,15 @@ def main():
 
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    train_data = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    train_data = datasets.CIFAR10(root='/data', train=True, download=True, transform=transform)
     train_loader = torch.utils.data.DataLoader(train_data, shuffle=True, **kwargs)
 
-    test_data = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    test_data = datasets.CIFAR10(root='/data', train=False, download=True, transform=transform)
     test_loader = torch.utils.data.DataLoader(test_data, shuffle=False, **kwargs)
 
     model = ConvNetwork(args.batch_size, args.train_mode, device).to(device)
     if args.train_mode == 'DKP':
+        # we need to run through the network once to properly initialize the backward weights
         test(model, device, test_loader, train_loader)
 
         forward_params = []
@@ -130,13 +132,13 @@ def main():
             else:
                 forward_params.append(param)
 
-        forward_optimizer = optim.SGD([{'params': forward_params}], lr=5e-4, weight_decay=1e-6, momentum=0.9, nesterov=True)
-        backward_optimizer = optim.Adam([{'params': backward_params}], lr=1e-4, weight_decay=1e-6)
+        forward_optimizer = optim.SGD([{'params': forward_params}], lr=args.lr, weight_decay=1e-6, momentum=0.9, nesterov=True)
+        backward_optimizer = optim.Adam([{'params': backward_params}], lr=args.b_lr, weight_decay=1e-6)
 
         optimizer = MultipleOptimizer(forward_optimizer, backward_optimizer)
-        scheduler = StepLR(backward_optimizer, step_size=1, gamma=0.80)
+        scheduler = StepLR(backward_optimizer, step_size=1, gamma=args.gamma)
     else:
-        optimizer = optim.SGD(model.parameters(), lr=5e-4, weight_decay=1e-6, momentum=0.9, nesterov=True)
+        optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=1e-6, momentum=0.9, nesterov=True)
 
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
