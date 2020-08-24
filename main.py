72ae89319951108f42bca54906e03b88922001c3
Matthew B. Webster <50574746+webstah@users.noreply.github.com>
2020-08-25 02:57:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -112,10 +112,10 @@ def main():
 
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    train_data = datasets.CIFAR10(root='/data', train=True, download=True, transform=transform)
+    train_data = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
     train_loader = torch.utils.data.DataLoader(train_data, shuffle=True, **kwargs)
 
-    test_data = datasets.CIFAR10(root='/data', train=False, download=True, transform=transform)
+    test_data = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     test_loader = torch.utils.data.DataLoader(test_data, shuffle=False, **kwargs)
 
     model = ConvNetwork(args.batch_size, args.train_mode, device).to(device)
